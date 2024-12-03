e979ac2cacf893cda254f6a283deb62c9929f6ca
kogxe@home <kogxe10@gmail.com>
2024-12-03 21:55:43 +0300

    adding second script for pw2

---
+++ PW2/pw2-2.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+
+#mkfs -t ext4 /dev/sda |grep UUID| awk '{print$3}'
+lsblk -o  name,type |grep part
+echo "Выбери диск /dev/:"
+
+read disctype
+
+echo "
+Выбери  тип файловой системы
+ext4
+ext3
+ext2
+xfs
+Тип файловой системы:
+"
+
+read fstype
+echo
+
+#partuuid=$(lsblk -o   name,partuuid  /dev/sdb1|awk '{print$2}' |tail -n 1)
+
+mkfs -t $fstype /dev/$disctype
+
+mount /dev/$disctype /storage
+
+partuuid=$(lsblk -o   name,partuuid  /dev/sdb1|awk '{print$2}' |tail -n 1)
+
+cat<<EOF>> /etc/fstab 
+#PARTUUID=$partuuid  /storage  $fstype defaults  0  2
+EOF
+
+
+ 
+echo  "
+Внимание!
+В целях безопасности изменения /etc/fstab закомментированы
+Необходимо перепроверить содержание файла  /etc/fstab
+Если диск был приммонтирован ранее ,и у него изменились  какие -либо значения
+необходимо закомментировать  строчку со старым значением и разкомментировать последнюю.
+После выполнить команду
+systemctl daemon-reload"
+
